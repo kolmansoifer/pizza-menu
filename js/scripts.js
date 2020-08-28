e5e1be488247deda4d90d804582d5c6cd6388833
kolmansoifer <kolmansoifer@gmail.com>
2020-08-28 10:38:57 -0700

    Add dipslay: none for h3 tag

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 function Pizza(size, topping) {
   this.size = size,
-    this.topping = topping,
+    this.topping = topping
 }
 
 Pizza.prototype.calculatePrice = function () {
   let pizzaPrice = parseInt(this.size);
   if (this.size === "1") {
-    pizzaPrice === 10;
+    pizzaPrice += 10;
   }
   else if (this.size === "2") {
     pizzaPrice += 5;
   }
   else {
-    pizzaPrice += 10;
+    pizzaPrice += 3;
   }
   return pizzaPrice;
 }
