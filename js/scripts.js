5af09a901d93d3d6b7650c825f9278505aa17147
kolmansoifer <kolmansoifer@gmail.com>
2020-08-30 18:49:51 -0500

    Adjust if else in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,42 @@
-function Pizza(size, topping) {
-  this.size = size,
-    this.topping = topping
+function Pizza(crust, topping, size) {
+  this.crust = crust
+  this.topping = topping
+  this.size = size
 }
 
-Pizza.prototype.calculatePrice = function () {
+Pizza.prototype.calcPrice = function () {
   let pizzaPrice = parseInt(this.size);
-  if (this.size === "3") {
-    pizzaPrice === 10;
-  }
-  else if (this.size === "2") {
+
+  if (this.crust === "3") {
     pizzaPrice += 5;
   }
+  else if (this.crust === "2") {
+    ticketCost += 2;
+  }
   else {
+    pizzaPrice === 10;
+  }
+
+  if (this.topping === "4" || this.topping === "5") {
     pizzaPrice += 3;
   }
-  if (this.topping === "4", "5", "6", "7") {
-    pizzaPrice += 2;
+  else if (this.topping === "6" || this.topping === "7") {
+    pizzaPrice += 1;
   }
+
   return pizzaPrice;
 }
 
 $(document).ready(function () {
-  $("#menu").submit(function (event) {
+  $("#form").submit(function (event) {
     event.preventDefault();
 
-    const pizzaSize = $("#pizza-size").val();
-    const pizzaToppings = $("#pizza-toppings").val();
+    const pizzaCrust = $("#crust").val();
+    const pizzaTopping = $("#topping").val();
+    const pizzaSize = $("#size").val();
 
-    let myPizza = new Pizza(pizzaSize, pizzaToppings);
-    let price = myPizza.calculatePrice();
+    let myPizza = new Pizza(pizzaCrust, pizzaTopping, pizzaSize);
+    let price = myPizza.calcPrice();
 
     $("#result").text(price);
     $("h3").show();
