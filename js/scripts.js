1eb4766166fb5fce6a989cbb2f8797de092fb377
kolmansoifer <kolmansoifer@gmail.com>
2020-08-30 18:58:25 -0500

    Add crust type to choices

--- js/scripts.js
+++ js/scripts.js
@@ -7,21 +7,27 @@ function Pizza(crust, topping, size) {
 Pizza.prototype.calcPrice = function () {
   let pizzaPrice = parseInt(this.size);
 
-  if (this.crust === "3") {
-    pizzaPrice += 5;
+  if (this.crust === "1") {
+    pizzaPrice += 0;
   }
   else if (this.crust === "2") {
-    ticketCost += 2;
+    ticketCost += 3;
   }
   else {
-    pizzaPrice === 10;
+    pizzaPrice += 5;
   }
 
-  if (this.topping === "4" || this.topping === "5") {
+  if (this.topping === "4") {
+    pizzaPrice += 0;
+  }
+  else if (this.topping === "5" || this.topping === "6") {
+    pizzaPrice += 1;
+  }
+  else if (this.topping === "7" || this.topping === "8") {
     pizzaPrice += 3;
   }
-  else if (this.topping === "6" || this.topping === "7") {
-    pizzaPrice += 1;
+  else {
+    pizzaPrice += 0;
   }
 
   return pizzaPrice;
